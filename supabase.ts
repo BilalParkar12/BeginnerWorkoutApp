30dd7f59bf921af91d6ac6c30a5bb24a3d36fbf0
Bilal Parkar <bilalparkar07@gmail.com>
2026-09-10 11:16:57 +0100

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 import 'expo-sqlite/localStorage/install'
 import 'react-native-url-polyfill/auto'
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
-const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_KEY!
+const supabaseUrl = import.meta.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabasePublishableKey = import.meta.env.EXPO_PUBLIC_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
   auth: {
     storage: localStorage,
